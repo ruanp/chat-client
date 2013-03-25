3a406ade0d3a511514f38f8e73b5f364e0cf475c
Hack Reactor Students <accounts@hackreactor.com>
2013-03-24 20:03:43 -0700

    WOW, Adding Messages, Showing Messages, Adding Rooms, (_NEEDS MESSAGE UPDATE FOR ROOM_)

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,6 @@ if(!/(&|\?)username=/.test(window.location.search)){
   if(newSearch !== '' & newSearch !== '?'){
     newSearch += '&';
   }
-  debugger;
   var ourUserName = (prompt('What is your name?') || 'anonymous');
   newSearch += 'username=' + ourUserName;
   window.location.search = newSearch;
@@ -20,33 +19,9 @@ var ourUserData = {
   "text" : "hello World"
 };
 
-//GET//
-// $.ajax({
-//   url: "https://api.parse.com/1/classes/messages",
-//   type: "GET",
-//   data: 'where={"username": "Andrew and Nima"}',
-//   crossDomain: true,
-//   contentType: "application/json"
-// }).done(function(stuff){
-//   console.log(stuff);
-// }).fail(function(){
-//   alert('fail');
-// });
-
-//POST//
-// $.ajax({
-//   url: "https://api.parse.com/1/classes/messages",
-//   type: "POST",
-//   data: JSON.stringify(ourUserData),
-//   crossDomain: true,
-//   contentType: "application/json"
-// }).done(function(stuff){
-//   console.log(stuff);
-// }).fail(function(){
-//   alert('you might as well kill yourself');
-// });
-
-
+var lastDate;
+var rooms = ['Default'];
+var thisRoom = 'Default';
 
 var getMessages = function(array) {
   var userObj = [];
@@ -63,11 +38,24 @@ var getMessages = function(array) {
   return userObj;
 };
 
+var switchRoom = function($roomItem) {
+
+//load all the wells in this room
+
+//set well refresh to only refresh wells from this room
+
+//unactivate the currently active room
+$('li.active').removeClass('active');
+//activate roomname
+$roomItem.addClass('active');
+
+};
+
 var makeMessageDiv = function(obj) {
 
-  $userP = $('<p class="text-right lead"></p>').text(obj.username);
-  $messageP = $('<p class="text-left"></p>').text(obj.msg);
-  $messageDiv = $('<div class="well"></div>').append($userP).append($messageP);
+  var $userP = $('<p class="text-right lead"></p>').text(obj.username);
+  var $messageP = $('<p class="text-left"></p>').text(obj.msg);
+  var $messageDiv = $('<div class="well"></div>').append($userP).append($messageP);
   return $messageDiv;
 };
 
@@ -75,27 +63,116 @@ var appendToMessages = function(div) {
   $('.msgHolder').append(div);
 };
 
-$.ajax({
-  url: "https://api.parse.com/1/classes/messages",
-  type: "GET",
-  crossDomain: true,
-  contentType: "application/json"
-}).done(function(response){
+var getter = function(date) {
+  var getParams = {
+      url: "https://api.parse.com/1/classes/messages",
+      type: "GET",
+      crossDomain: true,
+      contentType: "application/json"
+  };
   
-  var messageList = getMessages(response.results);
-  var $messageDiv;
+  var where = {}
 
-  for (var i =0; i < messageList.length; i++) {
-    $messageDiv = makeMessageDiv(messageList[i]);
-    appendToMessages($messageDiv);
+  where.updatedAt = 
+
+  var where = {
+    "updatedAt": {
+      "$gt": {{"__type":"Date","iso":"' + date + '"}}
+    },
+    "roomname": thisRoom
   }
 
 
-}).fail(function(){
-  alert('fail');
+  if (arguments.length > 0){
+    getParams.data = 'where={"updatedAt":{"$gt":{"__type":"Date","iso":"' + date + '"}}}';
+  }
+  if (arguments.length > 0 && thisRoom != 'Default') {
+    getParams.data = 'where={"roomname": "' + thisRoom + '"}';
+
+  }
+
+  $.ajax(getParams).done(function(response){
+      var messageList = getMessages(response.results);
+      var $messageDiv;
+      for (var i =0; i < messageList.length; i++) {
+        $messageDiv = makeMessageDiv(messageList[i]);
+        appendToMessages($messageDiv);
+      }
+      if (response.results.length > 0){
+        console.log(lastDate);
+        lastDate = response.results.pop().updatedAt;
+      }
+      setTimeout(getter, 5000, lastDate);
+  });
+
+};
+
+//ajax put
+var sendMessage = function(text, username){
+  $.ajax({
+    type: "POST",
+    url: "https://api.parse.com/1/classes/messages",
+    crossDomain: true,
+    contentType: "application/json",
+    data: JSON.stringify({"username": username, "text":text})
+  });
+};
+
+var addRoomtoArray = function(string) {
+  if (rooms.indexOf(string) === -1) {
+    rooms.push(string);
+    $('li.active').removeClass('active');
+    $('.nav.nav-list').append('<li class="active"><a href="#">' + string + '</a></li>');
+  }
+}
+
+$(document).ready(function() {
+
+$('.roomNameEnterer').hide();
+$('.submitNewRoom').hide();
+
+
+  $(".msgSend").click(function(event){
+    event.preventDefault();
+    var msgValue = $(".span12.search-query").val();
+    var userValue = $(".userName").val();
+    sendMessage(msgValue, userValue);
+    $(".span12.search-query").val("");
+    return false;
+  });
+
+  $(".msgHolder").on("click",'p.text-right.lead',(function(event) {
+    event.preventDefault();
+    newFriend = $(this).text();
+    console.log(newFriend);
+    $(".well").each(function() {
+      console.log(newFriend);
+      if (newFriend === $(this).children('.text-right.lead').text()) {
+        $(this).children('.text-left').toggleClass('friendMessage');
+        // $(this).children('.text-right.lead').prepend('<span class="label label-info friendLabel">friend</span>');
+      }
+    });
+  }));
+
+  $(".span2").on("click",".addRoomBtn", (function() {
+    $(this).hide();
+    $('.roomNameEnterer').show();
+    $('.submitNewRoom').show();
+  }));
+
+  $(".span2").on("click",".submitNewRoom", (function() {
+    
+    thisRoom = $('.roomNameEnterer').val();
+    addRoomtoArray(thisRoom);
+    $(this).hide();
+    $('.roomNameEnterer').hide();
+    $('.addRoomBtn').show();
+  }));
+
 });
 
 
+getter();
 
 
 
